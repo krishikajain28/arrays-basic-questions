b5b642e014b21e56841fe2d89dd9e4a12364b41d
Krishika Jain <krishikajain28@gmail.com>
2025-08-11 21:03:50 +0530

    modificatns

--- q9.cpp
+++ q9.cpp
@@ -1,33 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public: 
+class Solution
+{
+public:
+    // q9: moving all zeros at the end
 
-    // q9: moving all zeros at the end 
+    // bruteforce?
 
-    //bruteforce?
-    int movingAll0AtEnd(vector<int>& a, int n){
-    int i,j;
-    for (i = 0; i < n; i++){
-    
+    int movingAll0AtEnd(vector<int> &a, int n)
+    {
+        int i, j;
+        for (i = 0; i < n; i++)
+        {
+        }
     }
-}
 };
 
+int main()
+{
 
-int main(){
+    int n = 10;
+    vector<int> a = {3, 0, 45, 0, 97, 34, 0, 0, 11, 0};
+    // final vec: {3, 45, 97, 34, 11, 0, 0, 0, 0 }
 
-int n = 10;
-vector<int> a = {3, 0, 45, 0, 97, 34, 0, 0, 11, 0};
+    Solution s;
 
-Solution s;
+    cout << "Array after moviing all 0s at the end : ";
+    s.movingAll0AtEnd(a, n);
 
-cout << " Array after moviing all 0s at the end : ";
-s.movingAll0AtEnd( a,n);
-
-for (int i = 0; i < n; i++){
-    cout << a[i] << " ";
-}
-
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
 }
