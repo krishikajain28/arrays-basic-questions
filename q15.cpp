24de4547180f78167a3ab80e030960aa89a45dca
Krishika Jain <krishikajain28@gmail.com>
2025-11-05 14:43:07 +0530

    q15 b

--- q15.cpp
+++ q15.cpp
@@ -1,7 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// q15. find the numbers that appears once, and the others twice
+// meaning
+// u have an array like: {1,1,2,3,3,4,4,5,5}
+// the other numbers, other than 2 appears twice
+// but number 2 only appears once
+
+int once(vector<int> &a)
+
+{
+    int n = a.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        bool flag = false;
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j && a[i] == a[j])
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+        {
+            return a[i];
+        }
+    }
+
+    return -1; // fallback
+
+    // tc: O(n^2)
+    // sc: O(1)
+}
+
 int main()
 {
-    vector<int> a = {1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1};
+    vector<int> a = {1, 1, 2, 3, 3, 4, 4, 5, 5};
+
+    int m = once(a);
+
+    cout << "The numb appearing once in the array is: " << m;
 }
--- std_template.cpp
+++ std_template.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// q
+
+int once(vector<int> &a)
+{
+}
+
 int main()
 {
     vector<int> a = {1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1};
+
+    int m = once(a);
+
+    cout << "The numb appearing once in the array is: " << m;
 }
