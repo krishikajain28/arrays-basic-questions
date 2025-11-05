d384284c5dbf3c7f7ea24084d81349ea4eadfab1
Krishika Jain <krishikajain28@gmail.com>
2025-11-05 13:52:38 +0530

    q14. maximum consecutive 1s

--- q14.cpp
+++ q14.cpp
@@ -1,6 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// q14.
+// q14. maximum consecutive one's
+// means to find maximum consecutive 1's in an array
+// for ex : {1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1}
+// maximum consecutive ones: 3
 
-int main() {}
+int max1(vector<int> &a)
+{
+    int n = a.size();
+
+    int c = 0;
+    int m = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 1)
+        {
+            c++;
+            m = max(m, c); // max function says: keep m updated to the larger of the two: the current max streak (m) or the current running streak (c)
+        }
+        else
+        {
+            c = 0;
+        }
+    }
+    return m;
+}
+
+int main()
+{
+    vector<int> a = {1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1};
+
+    int mc = max1(a);
+    cout << "The max consc 1 in array is: " << mc;
+}
