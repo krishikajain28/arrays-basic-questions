725fb4f213dae758070269a2ba15a174e9327a3a
Krishika Jain <krishikajain28@gmail.com>
2025-11-09 09:45:23 +0530

    q2 started!!

---
+++ array/lec2/q2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// q2:
+
+int change_this_nameB(vector<int> &a)
+{
+    int n = size.a();
+}
+
+int change_this_nameBe(vector<int> &a)
+{
+    int n = size.a();
+}
+
+int change_this_nameO(vector<int> &a)
+
+{
+    int n = size.a();
+}
+
+int main()
+{
+    vector<int> a = {1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1};
+
+    int m = change_this_nameB(a);
+
+    cout << "The ans : " << m;
+}
